e53ca6244247c845a09470cbef331d8666b121e7
Wesley Bond <wesley.t.bond@gmail.com>
2023-06-23 20:36:12 -0400

    Updated code

--- quasar-project/contracts/Bet.sol
+++ quasar-project/contracts/Bet.sol
@@ -4,20 +4,25 @@ pragma solidity ^0.8.0;
 // Uncomment this line to use console.log
 
 contract Bet {
+    struct User {
+        uint256 escrow;
+        Transaction[] receipt;
+    }
     struct Transaction  {
         address addr;
-        uint amount;
-        uint team;
+        uint256 amount;
+        uint256 teamID;
+        uint256 gameID;
     }
     struct MoneyLine {
-        uint total;
+        uint256 total;
         mapping(uint => uint) teamTotal;
-        uint homeTeamID;
-        uint awayTeamID;
+        uint256 homeTeamID;
+        uint256 awayTeamID;
         address[] awayArray;
         address[] homeArray;
-        mapping(address => Transaction) receipt;
-        uint winner;
+        mapping(uint256 => mapping(address => Transaction)) receipt;
+        uint256 winner;
     }
      struct PointTotal {
         uint MoneyLine;
@@ -32,47 +37,43 @@ contract Bet {
         mapping(uint => mapping(string => Transaction[])) funders;
     }
 
-    mapping(address => uint) public balances;
+    mapping(address => User) public balances;
     mapping(uint256 => MoneyLine) public newMoneyLineBet;
     mapping(uint256 => uint256) public totalBetOnGame;
     mapping(uint256 => mapping(uint256 => uint256)) public totalBetOnTeam;
 
-function returnOdds(uint256 gameID, uint256 bettingTeamID, uint256 opposingTeamID) public view returns (uint256) {
-    return totalBetOnTeam[gameID][bettingTeamID] / (totalBetOnTeam[gameID][bettingTeamID] + totalBetOnTeam[gameID][opposingTeamID]);
-}
-function moneyLineBetCalculator(uint256 gameID, uint256 teamID, uint256 opposingTeamID, uint256 amount) public payable returns (uint256) {
-     uint256 currentTotal = newMoneyLineBet[gameID].teamTotal[teamID];
-    uint256 exampleNewTotal = currentTotal + amount;
-    uint256 payout = (amount/exampleNewTotal) * (exampleNewTotal + newMoneyLineBet[gameID].teamTotal[opposingTeamID]);
-    return payout;
-}
-function moneyLineBet(uint256 gameID, uint256 teamID, uint256 homeTeamID, uint256 awayTeamID) public payable returns (uint256) {
-    require(msg.value > 0, "You must bet something");
-    require(teamID == homeTeamID || teamID == awayTeamID, "You must bet on a team that is playing in this game");
-    uint256 currentGameTotal = totalBetOnGame[gameID];
-    uint256 currentGameTotalTeam = totalBetOnTeam[gameID][teamID];
-    uint256 currentTeamTotal = newMoneyLineBet[gameID].teamTotal[teamID];
-    uint256 currentTotal = newMoneyLineBet[gameID].total;
-
-    totalBetOnGame[gameID] = currentGameTotal + msg.value;
-    totalBetOnTeam[gameID][teamID] = currentGameTotalTeam + msg.value;
-    newMoneyLineBet[gameID].total =  currentTotal + msg.value;
-    newMoneyLineBet[gameID].teamTotal[teamID] = currentTeamTotal + msg.value;
-    newMoneyLineBet[gameID].homeTeamID = homeTeamID;
-    newMoneyLineBet[gameID].awayTeamID = awayTeamID;
-    newMoneyLineBet[gameID].receipt[msg.sender] = Transaction(msg.sender, msg.value, teamID);
-
-    if (teamID == homeTeamID) {
-        newMoneyLineBet[gameID].homeArray.push(msg.sender);
-        
-    } else {
-        newMoneyLineBet[gameID].awayArray.push(msg.sender);
+    function returnOdds(uint256 gameID, uint256 bettingTeamID, uint256 opposingTeamID) public view returns (uint256) {
+        return totalBetOnTeam[gameID][bettingTeamID] / (totalBetOnTeam[gameID][bettingTeamID] + totalBetOnTeam[gameID][opposingTeamID]);
     }
-   
-    return 0;
-}
+    function moneyLineBet(uint256 gameID, uint256 teamID, uint256 homeTeamID, uint256 awayTeamID) public payable {
+        require(msg.value > 0, "You must bet something");
+        require(teamID == homeTeamID || teamID == awayTeamID, "You must bet on a team that is playing in this game");
+        totalBetOnGame[gameID] += msg.value;
+        totalBetOnTeam[gameID][teamID] += msg.value;
+        newMoneyLineBet[gameID].total += msg.value;
+        newMoneyLineBet[gameID].teamTotal[teamID] += msg.value;
+        newMoneyLineBet[gameID].homeTeamID = homeTeamID;
+        newMoneyLineBet[gameID].awayTeamID = awayTeamID;
+        newMoneyLineBet[gameID].receipt[teamID][msg.sender] = Transaction(msg.sender, msg.value, teamID, gameID);
+        balances[msg.sender].escrow += msg.value;
+        balances[msg.sender].receipt.push(Transaction(msg.sender, msg.value, teamID, gameID));
+        if (teamID == homeTeamID) {
+            newMoneyLineBet[gameID].homeArray.push(msg.sender);
+        } else {
+            newMoneyLineBet[gameID].awayArray.push(msg.sender);
+        }
+       
+    }
 
+    function returnMoneyLineBetReceipt(uint256 gameID, uint256 teamID) public view returns (Transaction memory) {
+        return newMoneyLineBet[gameID].receipt[teamID][msg.sender];
+    }
 
+    function returnEscrow() public view returns (uint256) {
+        return balances[msg.sender].escrow;
+    }
 
-
+    function returnReceipts() public view returns (Transaction[] memory) {
+        return balances[msg.sender].receipt;
+    }
 }
--- quasar-project/src/pages/games/GameIndex.vue
+++ quasar-project/src/pages/games/GameIndex.vue
@@ -1,15 +1,60 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
+import { ref, watchEffect, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { ethers } from 'ethers';
 
 const router = useRoute();
-const gameTotal = ref();
 const provider = new ethers.BrowserProvider(window.ethereum);
 const contract = require('/artifacts/contracts/Bet.sol/Bet.json');
+
 const gameArray = ref([]);
+const gameTotal = ref();
+const homeTotalBet = ref();
+const awayTotalBet = ref();
+const homeOdds = ref();
+const awayOdds = ref();
 const statsArrayAway = ref([]);
 const statsArrayHome = ref([]);
+
+const homeDialog = ref(false);
+const awayDialog = ref(false);
+
+const homeBetType = ref();
+const homeMoneyline = ref();
+const homeSpread = ref();
+const homeTotal = ref();
+const awayBetType = ref();
+const awayMoneyline = ref();
+const awaySpread = ref();
+const awayTotal = ref();
+
+const returnGameTotal = computed(() => {
+  return gameTotal.value;
+});
+const returnHomeTotal = computed(() => {
+  return homeTotalBet.value;
+});
+
+const returnAwayTotal = computed(() => {
+  return awayTotalBet.value;
+});
+
+const returnHomeOdds = computed(() => {
+  return Number(homeOdds.value) * 100;
+});
+
+const returnAwayOdds = computed(() => {
+  return Number(awayOdds.value) * 100;
+});
+
+const returnAwayBetType = computed(() => {
+  return awayBetType.value;
+});
+
+const returnHomeBetType = computed(() => {
+  return homeBetType.value;
+});
+
 const statColumns = [
   {
     name: 'player',
@@ -55,19 +100,98 @@ watchEffect(async () => {
       );
     })
   );
-  if (gameTotal.value == undefined) {
-    const betContract = new ethers.Contract(
-      process.env.CONTRACT_ADDRESS,
-      contract.abi,
-      provider
-    );
+  const betContract = new ethers.Contract(
+    process.env.CONTRACT_ADDRESS,
+    contract.abi,
+    provider
+  );
 
+  if (gameTotal.value == undefined) {
     gameTotal.value = await betContract.totalBetOnGame(
       Number(router.params.id)
     );
-    console.log(gameTotal.value);
+  }
+  if (homeTotalBet.value == undefined) {
+    homeTotalBet.value = await betContract.totalBetOnTeam(
+      Number(router.params.id),
+      gameArray.value.home_team.id
+    );
+  }
+  if (awayTotalBet.value == undefined) {
+    awayTotalBet.value = await betContract.totalBetOnTeam(
+      Number(router.params.id),
+      gameArray.value.visitor_team.id
+    );
+  }
+  if (homeOdds.value == undefined) {
+    homeOdds.value = await betContract.returnOdds(
+      Number(router.params.id),
+      gameArray.value.home_team.id,
+      gameArray.value.visitor_team.id
+    );
+  }
+  if (awayOdds.value == undefined) {
+    awayOdds.value = await betContract.returnOdds(
+      Number(router.params.id),
+      gameArray.value.visitor_team.id,
+      gameArray.value.home_team.id
+    );
   }
 });
+
+const sendBetAway = async () => {
+  const signer = await provider.getSigner();
+  const betContract = new ethers.Contract(
+    process.env.CONTRACT_ADDRESS,
+    contract.abi,
+    signer
+  );
+  if (returnAwayBetType.value === 'moneyline') {
+    const overrides = {
+      value: awayMoneyline.value,
+    };
+    try {
+      const tx = await betContract.moneyLineBet(
+        Number(router.params.id),
+        gameArray.value.visitor_team.id,
+        gameArray.value.home_team.id,
+        gameArray.value.visitor_team.id,
+        overrides
+      );
+
+      console.log(tx);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+const sendBetHome = async () => {
+  const signer = await provider.getSigner();
+  const betContract = new ethers.Contract(
+    process.env.CONTRACT_ADDRESS,
+    contract.abi,
+    signer
+  );
+  if (returnHomeBetType.value === 'moneyline') {
+    const overrides = {
+      value: homeMoneyline.value,
+    };
+    try {
+      const tx = await betContract.moneyLineBet(
+        Number(router.params.id),
+        gameArray.value.home_team.id,
+        gameArray.value.home_team.id,
+        gameArray.value.visitor_team.id,
+        overrides
+      );
+
+      console.log(tx);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
 </script>
 <template>
   <q-page>
@@ -76,11 +200,11 @@ watchEffect(async () => {
         <q-card-section>
           <div class="row items-center justify-evenly text-h3 text-center">
             <div style="width: 40%">
-              {{ gameArray.visitor_team?.full_name }}
+              {{ gameArray.visitor_team?.full_name + ` (%${returnAwayOdds})` }}
             </div>
             <div style="width: 10%">@</div>
             <div style="width: 40%">
-              {{ gameArray.home_team?.full_name }}
+              {{ gameArray.home_team?.full_name + ` (%${returnHomeOdds})` }}
             </div>
           </div>
         </q-card-section>
@@ -109,7 +233,125 @@ watchEffect(async () => {
             class="row items-center justify-evenly text-center q-mx-auto"
           >
             <div>
-              <q-btn>Place Bet</q-btn>
+              <q-btn @click="awayDialog = true">{{
+                returnAwayTotal + ' ETH'
+              }}</q-btn>
+              <q-dialog
+                v-model="awayDialog"
+                persistent
+                maximized
+                transition-show="slide-up"
+                transition-hide="slide-down"
+              >
+                <q-card class="bg-grey-1 text-blue-grey-10">
+                  <q-toolbar>
+                    <q-avatar>
+                      <img
+                        src="https://content.sportslogos.net/logos/6/982/full/8147__national_basketball_association-primary-2018.png"
+                      />
+                    </q-avatar>
+
+                    <q-toolbar-title
+                      ><span class="text-weight-bold">EBSL</span>
+                    </q-toolbar-title>
+
+                    <q-btn flat round dense icon="close" v-close-popup />
+                  </q-toolbar>
+                  <q-card-section>
+                    <div class="text-h3 text-center">
+                      {{ gameArray.visitor_team?.full_name }}
+                    </div>
+                  </q-card-section>
+                  <div class="betTypes">
+                    <q-radio
+                      v-model="awayBetType"
+                      val="moneyline"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Money Line</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        Place your wager on who you think will win the game
+                      </q-card-section>
+                    </q-radio>
+                    <div v-if="awayBetType === 'moneyline'">
+                      <q-input
+                        style="max-width: 300px; margin: 0 auto 1em auto"
+                        square
+                        outlined
+                        label="ETH"
+                        type="number"
+                        v-model="awayMoneyline"
+                      ></q-input>
+                    </div>
+                  </div>
+
+                  <div class="betTypes">
+                    <q-radio
+                      v-model="awayBetType"
+                      val="spread"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Point Spread</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        The point spread is a calculated points difference
+                        determined by oddsmakers between two teams lining up.
+                      </q-card-section>
+                    </q-radio>
+                    <div v-if="awayBetType === 'spread'">
+                      <q-input
+                        style="max-width: 300px; margin: 0 auto 1em auto"
+                        square
+                        outlined
+                        label="ETH"
+                        type="number"
+                        v-model="awaySpread"
+                      ></q-input>
+                    </div>
+                  </div>
+                  <q-card class="betTypes">
+                    <q-radio
+                      v-model="awayBetType"
+                      val="total"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Point Total</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        You are placing a wager based on whether or not the two
+                        teams on the court exceed or fail to meet a certain
+                        combined score.
+                      </q-card-section>
+                      <div v-if="awayBetType === 'total'">
+                        <q-input
+                          style="max-width: 300px; margin: 0 auto 1em auto"
+                          square
+                          outlined
+                          label="ETH"
+                          type="number"
+                          v-model="awayTotal"
+                        ></q-input>
+                      </div>
+                    </q-radio>
+                  </q-card>
+                  <div class="text-center">
+                    <q-btn
+                      @click="async () => await sendBetAway()"
+                      label="Confirm"
+                    ></q-btn>
+                  </div>
+                </q-card>
+              </q-dialog>
             </div>
             <div class="row items-center justify-evenly text-center score">
               <div>
@@ -124,7 +366,7 @@ watchEffect(async () => {
                   {{ gameArray.time }}
                 </div>
                 <div>{{ gameArray.status }}</div>
-                <div>{{ `${gameTotal} ETH` }}</div>
+                <div>{{ `${returnGameTotal} ETH` }}</div>
               </div>
               <div>
                 <span>Home</span>
@@ -134,7 +376,125 @@ watchEffect(async () => {
               </div>
             </div>
             <div>
-              <q-btn>Place Bet</q-btn>
+              <q-btn @click="homeDialog = true">{{
+                returnHomeTotal + ' ETH'
+              }}</q-btn>
+              <q-dialog
+                v-model="homeDialog"
+                persistent
+                maximized
+                transition-show="slide-up"
+                transition-hide="slide-down"
+              >
+                <q-card class="bg-grey-1 text-blue-grey-10">
+                  <q-toolbar>
+                    <q-avatar>
+                      <img
+                        src="https://content.sportslogos.net/logos/6/982/full/8147__national_basketball_association-primary-2018.png"
+                      />
+                    </q-avatar>
+
+                    <q-toolbar-title
+                      ><span class="text-weight-bold">EBSL</span>
+                    </q-toolbar-title>
+
+                    <q-btn flat round dense icon="close" v-close-popup />
+                  </q-toolbar>
+                  <q-card-section>
+                    <div class="text-h3 text-center">
+                      {{ gameArray.home_team?.full_name }}
+                    </div>
+                  </q-card-section>
+                  <div class="betTypes">
+                    <q-radio
+                      v-model="homeBetType"
+                      val="moneyline"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Money Line</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        Place your wager on who you think will win the game
+                      </q-card-section>
+                    </q-radio>
+                    <div v-if="homeBetType === 'moneyline'">
+                      <q-input
+                        style="max-width: 300px; margin: 0 auto 1em auto"
+                        square
+                        outlined
+                        label="ETH"
+                        type="number"
+                        v-model="homeMoneyline"
+                      ></q-input>
+                    </div>
+                  </div>
+
+                  <div class="betTypes">
+                    <q-radio
+                      v-model="homeBetType"
+                      val="spread"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Point Spread</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        The point spread is a calculated points difference
+                        determined by oddsmakers between two teams lining up.
+                      </q-card-section>
+                    </q-radio>
+                    <div v-if="homeBetType === 'spread'">
+                      <q-input
+                        style="max-width: 300px; margin: 0 auto 1em auto"
+                        square
+                        outlined
+                        label="ETH"
+                        type="number"
+                        v-model="homeSpread"
+                      ></q-input>
+                    </div>
+                  </div>
+                  <div class="betTypes">
+                    <q-radio
+                      v-model="homeBetType"
+                      val="total"
+                      checked-icon="null"
+                      unchecked-icon="null"
+                    >
+                      <q-card-section>
+                        <div class="text-h6">Point Total</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        You are placing a wager based on whether or not the two
+                        teams on the court exceed or fail to meet a certain
+                        combined score.
+                      </q-card-section>
+                      <div v-if="homeBetType === 'total'">
+                        <q-input
+                          style="max-width: 300px; margin: 0 auto 1em auto"
+                          square
+                          outlined
+                          label="ETH"
+                          type="number"
+                          v-model="homeTotal"
+                        ></q-input>
+                      </div>
+                    </q-radio>
+                  </div>
+                  <div class="text-center">
+                    <q-btn
+                      @click="async () => await sendBetHome()"
+                      label="Confirm"
+                    ></q-btn>
+                  </div>
+                </q-card>
+              </q-dialog>
             </div>
           </div>
         </q-card-section>
@@ -195,5 +555,15 @@ watchEffect(async () => {
   padding: 2em;
   margin: auto;
   width: 50%;
+}
+.betTypes {
+  border: 3px $blue-grey-10 solid;
+  text-align: center;
+  margin: 1em auto;
+  width: 80%;
+}
+
+input[type='radio'] .betTypes {
+  display: none;
 }
 </style>
