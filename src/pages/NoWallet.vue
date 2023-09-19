7ddb015f7f70201d629962c17940cec7d79e36b4
Wesley Bond <wesley.t.bond@gmail.com>
2023-09-19 01:01:13 -0400

    update

--- src/pages/NoWallet.vue
+++ src/pages/NoWallet.vue
@@ -1,6 +1,11 @@
 <template>
   <q-card class="full-height full-width">
     <div class="fixed-center">
+      <q-card-section>
+        <div class="text-center text-blue ebslTitle">
+          ETHEREUM BASKETBALL SPECULATION LEAGUE
+        </div>
+      </q-card-section>
       <q-card-section class="row items-center justify-center">
         <q-avatar>
           <q-img
@@ -8,15 +13,22 @@
             fit="contain"
           />
         </q-avatar>
-        <span class="text-h4 text-bold">EBSL</span>
+        <span class="text-h3 text-bold text-blue-grey-10">EBSL</span>
       </q-card-section>
       <q-card-section class="text-center">
         <q-img
           width="250px"
           src="https://yt3.ggpht.com/a-/AAuE7mC1z-HXEKxL4YhAhc7WDHWA6Rnly1I592T5ag=s900-mo-c-c0xffffffff-rj-k-no"
         />
-        <div class="text-h6">Please Install MetaMask!</div>
+        <div class="text-h6">
+          Please Install <span class="text-orange-10">MetaMask</span>!
+        </div>
       </q-card-section>
     </div>
   </q-card>
 </template>
+<style lang="scss" scoped>
+.ebslTitle {
+  font-size: 0.8em;
+}
+</style>
